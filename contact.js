56fe4e6abfcf1f3be2a07c9711addc7f8a623345
Kingsly62 <clinton.makori@student.moringaschool.com>
2021-11-30 23:52:55 +0300

    Add mailchimp validation

--- contact.js
+++ contact.js
@@ -10,13 +10,18 @@ var comment = $('#comment').val();
 $(".error").remove();
 
 if(name.length<1){
-    $('#name').after('<span>Provide your name</span>');
+    //$('#name').after('<span>Provide your name</span>');
+    alert("Provide your valid official fullnames");
 }
 if(email.length<1){
-    $('#email').after('<span>Provide your email account</span>')
+    //$('#email').after('<span>Provide your email account</span>')
+    alert("Provide your valid email to continue");
 }
 if(comment.length<1){
-    $('#comment').after('<span>write your comment kindly</span>')
+    //$('#comment').after('<span>write your comment kindly</span>')
+    alert("Provide your comment kindly to submit");
+}else{
+    alert('We have received your message successfully.We value you alot.Continue trading with our platform Thanks!!!!');
 }
-})
-})
+});
+});
