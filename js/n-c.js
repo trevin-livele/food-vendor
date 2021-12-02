cad8377971ceba0b7e385f710de4159cba32684a
njeri waweru <wawerunjeri714@gmail.com>
2021-12-02 07:22:29 +0300

    changes in html js and css

---
+++ js/n-c.js
@@ -0,0 +1,26 @@
+// $(document).ready(function () {
+//     $(".panuel").click(function () {
+//       $(".kennedy").toggle();
+//       $(".panuel").hide();
+//       $(".panuel,kennedy .").click(function () {
+//         $(".panuel").show();
+//         $(".one").hide();
+//       });
+//     });
+//     $(".development-icon").click(function () {
+//       $(".development-description").toggle();
+//       $(".development-icon").hide();
+//       $(".development, .development-description").click(function () {
+//         $(".development-description").hide();
+//         $(".development-icon").show();
+//       });
+//     });
+//     $(".product-icon").click(function () {
+//       $(".product-description").toggle();
+//       $(".product-icon").hide();
+//       $(".product, .product-description").click(function () {
+//         $(".product-icon").show();
+//         $(".product-description").hide();
+//       });
+//     });
+//   });
